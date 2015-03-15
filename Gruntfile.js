ef1a435cff4fadcdad85af81237fb1bec87212e6
Mariusz Rubin <mariusz.rubin@gmail.com>
2015-03-15 22:36:16 +0100

    prod build

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,24 +3,51 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: ['bin'],
     uglify: {
+      prod: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+          mangle: true,
+          compress: true
+        },
+        files: {
+          'bin/<%= pkg.name %>.min.js' : ['src/js/*.js']
+        }
+      }
+    },
+    processhtml: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        mangle: true,
-        compress: true
+        data: {
+          message: 'Configure html for prod'
+        }
       },
-      build: {
-        src: 'src/js/*.js',
-        dest: 'bin/<%= pkg.name %>.min.js',
-        
+      dist: {
+        files: {
+          'bin/index.html': ['src/index.html']
+        }
+      }
+    },
+    copy: {
+      css: {
+        src: 'src/style.css',
+        dest: 'bin/style.css'
+      },
+      jslib: {
+        src: 'src/js/lib/*',
+        dest: 'bin/lib/',
+        filter: 'isFile',
+        expand: true,
+        flatten: true
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask('default', ['clean', 'uglify', 'processhtml', 'copy']);
 };
